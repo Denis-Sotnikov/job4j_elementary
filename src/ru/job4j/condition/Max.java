ee60b31aff3930347a6c3983583250d771f590fd
Denchik-1988 <sport_piter@mail.ru>
2020-11-22 10:23:08 +0300

    4.2. Тернарное сравнение [#413630]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,12 +2,12 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean flag = left > right;
+        boolean flag = left >= right;
         int result = flag ? left : right;
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(max(10, 15));
+        System.out.println(max(15, 15));
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -12,4 +12,16 @@ public class MaxTest {
         int result = Max.max(1, 2);
         assertThat(result, is(2));
     }
+
+    @Test
+    public void whenMax3To1Then3() {
+        int result = Max.max(3, 1);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax5To5Then5() {
+        int result = Max.max(5, 5);
+        assertThat(result, is(5));
+    }
 }
