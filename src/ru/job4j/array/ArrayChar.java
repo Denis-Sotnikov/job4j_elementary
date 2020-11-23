0aac3bf253eb9eeaf8e1343feac5df7d4be58ebf
Denchik-1988 <sport_piter@mail.ru>
2020-11-24 02:27:44 +0300

    6.4. Слово начинается с ... [#413639]

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        int size = word.length <= pref.length ? word.length : pref.length;
+        for (int i = 0; i < size; i++) {
+                       if (word[i] != pref[i])
+                           return false;
+        }
+        return result;
+    }
+}
