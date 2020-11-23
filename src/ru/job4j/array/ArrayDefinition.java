0ed4ff27538aa6e012a4182c8c9cbd89a0db8131
Denchik-1988 <sport_piter@mail.ru>
2020-11-23 08:06:50 +0300

    6.0.1.1. Размер массива [#413662]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,10 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println(ages.length);
         String[] sumames = new String[100500];
+        System.out.println(sumames.length);
         float[] prices = new float[40];
+        System.out.println(prices.length);
     }
 }
