5e3cf5033748d06d4586480cc2d6f89f8e7e2659
Denchik-1988 <sport_piter@mail.ru>
2020-11-23 08:12:21 +0300

    6.0.2. Заполнение массива. [#413651]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println(sumames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Ivan";
+        names[1] = "Olya";
+        names[2] = "Viktoria";
+        names[3] = "Mariya";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
