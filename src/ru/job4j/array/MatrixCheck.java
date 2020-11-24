b33c87bbca74c15cf806701785ae7763d6e9fe0b
Denchik-1988 <sport_piter@mail.ru>
2020-11-25 00:05:45 +0300

    6.7.4. Выигрышные комбинации в сокобан [#413641]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -40,4 +40,22 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X') {
+                if (monoHorizontal(board, i)) {
+                    result = true;
+                    break;
+                }
+                if (monoVertical(board, i)) {
+                    result = true;
+                    break;
+                }
+
+            }
+        }
+        return result;
+    }
 }
