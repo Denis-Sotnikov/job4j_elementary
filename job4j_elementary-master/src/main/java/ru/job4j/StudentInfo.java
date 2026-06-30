915c2e827a24aba1b5dce955b6461eddd6708cc6
sport <sport_pite@mail.ru>
2026-07-01 00:58:31 +0300

    Заливка файла Students

---
+++ job4j_elementary-master/src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package main.java.ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sotnikov Denis");
+        System.out.println("17.08.1988");
+    }
+}
